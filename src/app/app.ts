6eea34f72a1e4a8f0e99a18286bc22921ec41982
Kaique Barbosa Dultra <kaique.dultra@outlook.com>
2026-02-22 13:37:19 -0300

    Login page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,51 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [
+    ReactiveFormsModule
+  ]
 })
-export class App {
-  protected readonly title = signal('fluxo-de-caixa-ui');
+export class AppComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [
+        Validators.required,
+        Validators.pattern(/^[0-9]{10,11}$/),
+        Validators.minLength(10),
+        this.onlyNumbersPhone
+      ]]
+    });
+  }
+
+  onlyNumbersPhone(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (value && !/^\d+$/.test(value)) {
+      return { onlyNumbers: true}
+    }
+    return null;
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Dados do formulário:', this.form.value);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 }
