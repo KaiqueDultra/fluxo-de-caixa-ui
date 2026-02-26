dcfae6659e2d0565117b10ba671b8c58da9ae971
Kaique Barbosa Dultra <kaique.dultra@outlook.com>
2026-02-26 19:16:15 -0300

    add: Create dashboard page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import {
-  AbstractControl,
-  FormBuilder,
-  FormGroup,
   ReactiveFormsModule,
-  ValidationErrors,
-  Validators
 } from '@angular/forms';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,38 +10,10 @@ import {
   templateUrl: './app.html',
   styleUrl: './app.scss',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterOutlet
   ]
 })
 export class AppComponent {
-  form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      nome: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      telefone: ['', [
-        Validators.required,
-        Validators.pattern(/^[0-9]{10,11}$/),
-        Validators.minLength(10),
-        this.onlyNumbersPhone
-      ]]
-    });
-  }
-
-  onlyNumbersPhone(control: AbstractControl): ValidationErrors | null {
-    const value = control.value;
-    if (value && !/^\d+$/.test(value)) {
-      return { onlyNumbers: true}
-    }
-    return null;
-  }
-
-  onSubmit() {
-    if (this.form.valid) {
-      console.log('Dados do formulário:', this.form.value);
-    } else {
-      this.form.markAllAsTouched();
-    }
-  }
 }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators
+} from "@angular/forms";
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [
+        Validators.required,
+        Validators.pattern(/^[0-9]{10,11}$/),
+        Validators.minLength(10),
+        this.onlyNumbersPhone
+      ]]
+    });
+  }
+
+  onlyNumbersPhone(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (value && !/^\d+$/.test(value)) {
+      return { onlyNumbers: true}
+    }
+    return null;
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Dados do formulário:', this.form.value);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+}
