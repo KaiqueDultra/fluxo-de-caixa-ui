5fe4d5315a0bea1fd1be99f17a6036b6e3776c05
Kaique Barbosa Dultra <kaique.dultra@outlook.com>
2026-03-19 18:49:46 -0300

    add: adding logic to download the sheet file

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,16 +9,55 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+
+  modalAberto = false;
+  isDownloading = false;
+
   toastVisible = false;
   toastMessage = '';
 
-  showPlanilhaToast() {
-    this.toastMessage = 'Planilha não disponível para download';
-    this.toastVisible = true;
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  abrirModal() {
+    this.modalAberto = true;
+    document.body.style.overflow = 'hidden';
   }
 
-  closeToast() {
-    this.toastVisible = false;
+  fecharModal() {
+    this.modalAberto = false;
+    document.body.style.overflow = 'auto';
+  }
+
+  confirmarDownload() {
+    if (this.isDownloading) return;
+
+    this.isDownloading = true;
+
+    const link = document.createElement('a');
+    link.href = 'assets/files/Fluxo_de_Caixa.xlsx';
+    link.download = 'fluxo-de-caixa.xlsx';
+    link.click();
+
+    setTimeout(() => {
+      this.modalAberto = false;
+      this.isDownloading = false;
+      document.body.style.overflow = 'auto';
+
+      this.cdr.detectChanges();
+
+      this.showToast('Download da planilha realizado com sucesso!');
+    }, 3000);
+  }
+
+  showToast(message: string) {
+    this.toastMessage = message;
+    this.toastVisible = true;
+
+    this.cdr.detectChanges();
+
+    setTimeout(() => {
+      this.toastVisible = false;
+      this.cdr.detectChanges();
+    }, 6000);
   }
 }
-
